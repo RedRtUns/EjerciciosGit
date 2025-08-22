c14dcc9099acfcbe7b8f2258669169536550e37b
William S <will107673@gmail.com>
2025-08-22 17:10:37 -0500

    PRimeros datos

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("William David Suarez Orjuela");
+        System.out.println("Estudiante de Ingenieria en sistemas");
+
+    }
+}
