25a036d893339358f72927f29e5f2858e9d16107
William.S <will107673@gmail.com>
2025-08-28 22:35:25 -0500

    Prueba Git add .

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         System.out.println("Hola mundo");
         System.out.println("William David Suarez Orjuela");
         System.out.println("Estudiante de Ingenieria en sistemas");
+        System.out.println("Ensayo")
+        System.out.println("Git add .");
 
     }
 }
