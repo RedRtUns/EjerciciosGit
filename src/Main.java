58dc49b4f313d0ea4f33df8ae3c1cb849d999583
William.S <will107673@gmail.com>
2025-08-29 14:20:28 -0500

    Cambios nuevos

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ public class Main {
         System.out.println("Hola mundo");
         System.out.println("William David Suarez Orjuela");
         System.out.println("Estudiante de Ingenieria en sistemas");
-        System.out.println("Ensayo")
+        System.out.println("Ensayo");
         System.out.println("Git add .");
 
         System.out.println("Git switch -c Nombre nuevo de la rama");
         System.out.println("Git checkout main ");
         System.out.println("Git Pull origin main");
+        System.out.println("Git commit -m");
     }
 }
